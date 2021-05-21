d6526ac7cb875dae05a11f3c4692970121cfe676
layanabushaweesh <layanabushaweesh@gmail.com>
2021-05-21 18:24:13 +0300

    ya 3eni 3lek

--- schema.sql
+++ schema.sql
@@ -1,13 +1,10 @@
-DROP TABLE IF EXISTS characters;
-
-CREATE TABLE characters (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    house VARCHAR(255),
-    patronus VARCHAR(255),
-    is_alive boolean NOT NULL DEFAULT true,
-    image VARCHAR(255),
-    yearOfBirth INTEGER NOT NULL ,
-    created_by VARCHAR(255)
+CREATE TABLE IF NOT EXISTS
+users (
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(255),
+ house VARCHAR(255),
+patronus VARCHAR(255),
+alive BOOLEAN DEFAULT true ,
+created_by VARCHAR(255)
 );
 
--- server.js
+++ server.js
@@ -21,115 +21,118 @@ app.use(express.static('public'))
 // define our method-override reference
 app.use(methodOverride('_method'))
 // Set the view engine for server-side templating
-app.set('view engine' , 'ejs')
+app.set( 'view engine' , 'ejs')
 // Use app cors
 app.use(cors())
-
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
-
+// const client = mew pg.Client(process.env.DATABASE_URL)
 // app routes here
 // -- WRITE YOUR ROUTES HERE --
 app.get('/home' , renderHome)
-app.post('/favorite-character', saveCharacter)
-app.get('/character/my-fav-characters',renderFav)
+app.post('/save' , saveData)
+app.get('/character/my-fav-characters' , renderFav)
 
-app.post('/character/:character_id' , showDetalis)
-app.put('/character/:character_id', updateQ)
-app.delete('/character/:character_id', deleteQ)
-// creating routs
-// app.post('/character/create', createCharacter)
-// app.get('/character/my-characters', showCreate)
-// app.get('/character/create' , renderCreate)
+app.post('/character/:character_id' , shawDetalis)
+app.put('/character/:character_id' , updateCh)
+app.delete('/character/:character_id' , deletCh)
+
+app.post('/character/create' , createCh)
+app.get('/character/create' , renderCreate)
+app.get('/character/my-fav-characters' , renderCreated)
 // callback functions
+
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
 function renderHome(req,res) {
     const url='http://hp-api.herokuapp.com/api/characters'
-    superagent.get(url).then((dataFromApi)=>{
+    superagent.get(url).then((dataFromApi =>{
         // console.log(dataFromApi.body)
-        const apiData = dataFromApi.body.map(data =>{
-            return new Character(data)
+        const apiData =dataFromApi.body.map(data =>{
+            return new Charachter(data)
         })
-        res.render('index' , {characters : apiData})
-    }).catch(error =>(console.log(error)))
-    
-}
-
-function saveCharacter(req,res) {
-const {name,house,patronus,alive,image,yearOfBirth} = req.body
-const sql ='INSERT INTO  characters (name,house,patronus,is_alive,image,yearOfBirth,created_by) VALUES ($1,$2,$3,$4,$5,$6,$7)'
-  const value =[name,house,patronus,alive,image,yearOfBirth,'api']  
-  client.query(sql,value).then(()=>{
-      res.redirect('/character/my-fav-characters')
-  }).catch(error =>{console.log(error)})
-}
-function renderFav(req,res) {
-
-    const sql ='SELECT * FROM characters WHERE created_by=$1'
-    const value=['api']
-    client.query(sql,value).then((dataFromDB =>{
-        res.render('fav',{ fav : dataFromDB.rows})
+        res.render( 'index' , { allData : apiData})
     })).catch(error =>{console.log(error)})
     
 }
-function showDetalis(req,res) {
-    const id=req.params.character_id
-    const sql ='SELECT * FROM characters WHERE id=$1;'
-    const value =[id]
-    
-    client.query(sql,value).then((dataFromDB) =>{
-        res.render('detalis' , { datalis : dataFromDB.rows})
-    }).catch(error =>(console.log(error)))
-}
 
-function updateQ(req,res) {
-    const id =req.params.character_id
-    const{name,house,patronus,status,yearOfBirth} =req.body   
-    const sql ='UPDATE characters SET name=$1 house=$2 patronus=$3 is_alive=$4 yearOfBirth=$5 WHERE id=$6;'
-    const value=[name,house,patronus,status,yearOfBirth,id]
-    client.query(sql,value).then(()=>{
-        res.redirect(`/character/${id}`)
-    }).catch(error =>{console.log(error)})
-}
-function deleteQ(req,res) {
-    const id =req.params.character_id
-    const sql ='DELETE FROM characters WHERE id=$1'
-    const value =[id]
+function saveData(req,res) {
+    const {name,house,patronus,alive} = req.body
+    const sql =' INSERT INTO users (name,house,patronus,alive,created_by) VALUES ($1,$2,$3,$4,$5)'
+    const value =[name,house,patronus,alive,'api']
     client.query(sql,value).then(()=>{
         res.redirect('/character/my-fav-characters')
     }).catch(error =>{console.log(error)})
 }
 
-// function createCharacter(req,res) {
-//     const {name,house,patronus,status} =req.body
-//     const sql ='INSERT INTO characters (name,house,patronus,is_alive,created_by) VALUES ($1,$2,$3,$4,$5)'
-//     const value =[name,house,patronus,status,'user']
-//     client.query(sql,value).then(()=>{
-//         res.redirect('/character/my-characters')
-//     }).catch(error =>{console.log(error)})
-// }
-// function showCreate(req,res) {
-//     const sql ='SELECT * FROM characters WHERE created_by=$1'
-//     const value=['user']
-//     client.query(sql,value).then((dataFromDB)=>{
-//         res.render('fav',{ fn : dataFromDB.rows})
-
-//     })
-// }
-// function renderCreate(req,res) {
-//     res.render('create-chara')
-// }
-// helper functions
-function Character(data) {
+function renderFav(req,res) {
+  const sql ='SELECT * FROM users WHERE created_by=$1'  
+  const value=['api']
+  client.query(sql,value).then((dataFromDb =>{
+      res.render('fav' , { fav : dataFromDb.rows})
+  })).catch(error =>{console.log(error)})
+}
+function shawDetalis(req,res) {
+    const id =req.params.character_id
+    const sql ='SELECT * FROM users WHERE id=$1'
+    const value= [id]
+    client.query(sql,value).then((dataFromDb)=>{
+        res.render('detalis' , {detalis : dataFromDb.rows})
+    }).catch(error =>{console.log(error)})
     
+}
+function updateCh(req,res) {
+    const id =req.params.character_id
+    const {name,house,patronus,staus} = req.body
+    const sql =' UPDATE users SET name=$1 , house=$2 , patronus=$3 , alive=$4 WHERE id=$5' 
+    const value =[name,house,patronus,staus,id]
+    client.query(sql,value).then(()=>{
+        res.redirect('/character/my-fav-characters')
+    }).catch(error =>{console.log(error)})
+    
+}
+function deletCh(req,res) {
+    const id = req.params.character_id
+    const sql ='DELETE FROM users WHERE id=$1'
+    const value =[id]
+    client.query(sql,value).then(()=>{
+        res.redirect('/character/my-fav-characters')
+    }).catch(error =>{console.log(error)})
+    
+}
+
+function createCh(req,res) {
+    const {name,house,patronus,staus} = req.body
+    const sql ='INSERT INTO users (name,house,patronus,alive,created_by) VALUES ($1,$2,$3,$4,$5)'
+    const value=[name,house,patronus,staus,'user']
+    client.query(sql,value).then(()=>{
+        res.redirect('/character/my-characters')
+    }).catch(error =>{console.log(error)})
+    
+}
+function renderCreate(req,res) {
+    res.render('create')
+    
+}
+function renderCreated(req,res) {
+    const sql = 'SELECT * FROM users WHERE created_by = $1' 
+    const value =['user']
+    client.query(sql,value).then((dataFromDb)=>{
+        res.render('fav' , { fav : dataFromDb.rows})
+    }).catch(error =>{console.log(error)})
+    
+}
+// helper functions
+function Charachter(data) {
    this.name=data.name
-   this. house=data.house
+   this.house=data.house
    this.patronus=data.patronus
    this.alive=data.alive
-   this.image=data.image
-   this.yearOfBirth=data.yearOfBirth
 }
 // app start point
-client.connect().then(() =>
-    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
-);
+// client.connect().then(() =>
+//     app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+// );
+
+client.connect().then(()=>{
+    app.listen( PORT , () =>(console.log(`listen ${PORT}`)))
+})
